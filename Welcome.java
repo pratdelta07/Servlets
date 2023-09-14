4ab69db7b7eef53b082a4ea0cca4ab528eab6e10
pratdelta07 <144924496+pratdelta07@users.noreply.github.com>
2023-09-14 19:47:34 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,50 @@
+package com.infinite.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Welcome() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if (request.getParameter("username").equals("alpha")){
+			if (request.getParameter("password").equals("alpha")){
+				response.sendRedirect("success.html");
+			}
+			else{
+				response.sendRedirect("failure.html");
+			}
+		}
+		else{
+			response.sendRedirect("failure.html");
+		}
+
+	}
+
+}
