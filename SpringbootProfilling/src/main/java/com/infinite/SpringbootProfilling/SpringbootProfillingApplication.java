93aff982e3408c908077bbfb0015ac69580b225f
pratdelta07 <144924496+pratdelta07@users.noreply.github.com>
2023-10-25 12:04:22 +0530

    This is commit

---
+++ SpringbootProfilling/src/main/java/com/infinite/SpringbootProfilling/SpringbootProfillingApplication.java
@@ -0,0 +1,14 @@
+package com.infinite.SpringbootProfilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProfillingApplication.class, args);
+	}
+
+}
+
