d9971e925e8d5125bde1096bfb3f98dfeda3c575
pratdelta07 <144924496+pratdelta07@users.noreply.github.com>
2023-10-30 09:01:54 +0530

    This is Commit

---
+++ Mdiscovery/src/test/java/com/eureka/demo/MdiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.eureka.demo;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdiscoveryApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
+*/
